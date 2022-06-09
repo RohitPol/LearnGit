a0817d668e6bc0534368033c4be062d8e3e965d0
Rohit Pol <rohitpol1258@gmail.com>
2022-06-09 15:38:11 +0530

    Tickit 111 : Modification is done 	modified:   Maths/factorial.c 	modified:   Maths/sum.c

--- Maths/factorial.c
+++ Maths/factorial.c
@@ -2,6 +2,8 @@
 
 int factorial(int n);
 
+long factorial(long n);
+
 int factorial(int n)
 {
 	if( n == 1 || n == 0)
@@ -12,6 +14,11 @@ int factorial(int n)
 	{
 		return n + factorial(n-1)
 	}
+}
+
+long factorial(long n)
+{
+
 }
 
 
--- Maths/sum.c
+++ Maths/sum.c
@@ -2,7 +2,14 @@
 
 int add(int x, int y);
 
+int add(int x, int y, int z);
+
 int add(int x. int y)
 {
 	return x + y;
+}
+
+int add(int x, int y, int z)
+{
+	return x+y+z;
 }
