4bdb9502d4b7ccf2830493a4bb6916a24a777cc1
Rohit Pol <rohitpol1258@gmail.com>
2022-06-09 17:19:56 +0530

    Feature 116 : Add limit and Derivative 	new file:   Calculus/Derivative.cpp 	new file:   Calculus/Limits.c

---
+++ Calculus/Derivative.cpp
@@ -0,0 +1,9 @@
+#include<iostrem>
+using namespace std;
+
+void derivitive(int a);
+
+void derivitive(int a)
+{
+	cout << "Derivative";
+}
---
+++ Calculus/Limits.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+void limit(int n);
+
+void limit(int n)
+{
+	printf("Limit");
+}
